e26846d66846cd402bb10e4043761f67ba5e6f3d
Babalo Nogqala <babalonogqala23@gmail.com>
2024-09-03 17:25:36 +0000

    fix file name

--- WPF-CMCS/WPF-CMCS/MainWindow.xaml.cs
+++ WPF-CMCS/WPF-CMCS/MainWindow.xaml.cs
@@ -11,25 +11,37 @@ using System.Windows.Shapes;
 
 namespace WPF_CMCS
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            PopulateRecentClaims();
         }
+
+        private void DarkModeToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            ApplyDarkMode();
+        }
+
+        private void DarkModeToggle_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ApplyLightMode();
+        }
+
         private void ApplyDarkMode()
         {
             this.Background = new SolidColorBrush(Color.FromRgb(18, 18, 18));
-            
+            // Apply dark mode styles to other controls
         }
+
         private void ApplyLightMode()
         {
             this.Background = new SolidColorBrush(Colors.White);
-           
+            // Apply light mode styles to other controls
         }
 
     }
 }
+        
