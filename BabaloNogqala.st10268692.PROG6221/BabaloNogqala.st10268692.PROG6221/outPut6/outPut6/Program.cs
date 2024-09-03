6762a23259ba73ae13a10292ec66ddc302bad5d1
Babalo Nogqala <babalonogqala23@gmail.com>
2024-09-03 17:06:47 +0000

    Add project files.

---
+++ BabaloNogqala.st10268692.PROG6221/BabaloNogqala.st10268692.PROG6221/outPut6/outPut6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter 6 Values:");
+
+int count = 0;
+while (count < 6)
+{
+    Console.Write($"Value {count + 1}: ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Entered Value: {value}");
+    count++;
+}
+
